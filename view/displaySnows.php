cac6110b638e488d29c9af0afa0b7d096f40e6d0
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-01-17 20:01:58 +0100

    Seul les snows disponibles sont affichés

--- view/displaySnows.php
+++ view/displaySnows.php
@@ -5,11 +5,13 @@ ob_start();
 ?>
 <h1>List des Snowboards</h1>
 
-<?php foreach ($snows as $snow) { ?>
-    <hr>
-    <img src="view/images/snows/<?= $snow['smallimage'] ?>">
-    <h2><?= $snow['modele'] ?></h2>
-<?php } ?>
+<?php foreach ($snows as $snow) {
+    if ($snow["disponible"] == true){ ?>
+        <hr>
+        <img src="view/images/snows/<?= $snow['smallimage'] ?>">
+        <h2><?= $snow['modele'] ?></h2>
+<?php }
+} ?>
 
 <?php
 $content = ob_get_clean();
