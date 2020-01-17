fb846f7bd1fcdd5b8a1924fe8e1a0b3ed133093b
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-01-17 15:00:46 +0100

    Affichage des images des snows

--- view/displaySnows.php
+++ view/displaySnows.php
@@ -6,6 +6,8 @@ ob_start();
 <h1>List des Snowboards</h1>
 
 <?php foreach ($snows as $snow) { ?>
+    <hr>
+    <img src="view/images/snows/<?= $snow['smallimage'] ?>">
     <h2><?= $snow['modele'] ?></h2>
 <?php } ?>
 
