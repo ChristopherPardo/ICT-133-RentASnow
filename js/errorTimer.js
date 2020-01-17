c9339b878aae81d094548a71592bf6bddd8b75c4
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-01-17 14:52:54 +0100

    List des modele de snows dans la page éponyme

--- js/errorTimer.js
+++ js/errorTimer.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-    flashmessage.addEventListener("load", setTimeout("errorTimer()",3000))
+    setTimeout("errorTimer()",3000)
 }
 
 function errorTimer() {
--- view/displaySnows.php
+++ view/displaySnows.php
@@ -1,4 +1,15 @@
 <?php
 $title = "Display Snows";
+$snows = getSnows();
+ob_start();
+?>
+<h1>List des Snowboards</h1>
+
+<?php foreach ($snows as $snow) { ?>
+    <h2><?= $snow['modele'] ?></h2>
+<?php } ?>
+
+<?php
+$content = ob_get_clean();
 require "gabarit.php";
 ?>
