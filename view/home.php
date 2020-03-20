a3633f1a77354a810f38ee0633c516d1776898d7
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-03-20 01:22:55 +0100

    Formulaire de création d'un commentaire

--- view/home.php
+++ view/home.php
@@ -16,7 +16,8 @@ $title = "RentASnow - Accueil";
                 <div class="camera_caption fadeFromBottom cap1">Les derniers modèles toujours à disposition.</div>
             </div>
             <div data-src="view/images/slider/1.jpg">
-                <div class="camera_caption fadeFromBottom cap2">Découvrez des paysages fabuleux avec des sensations.</div>
+                <div class="camera_caption fadeFromBottom cap2">Découvrez des paysages fabuleux avec des sensations.
+                </div>
             </div>
             <div data-src="view/images/slider/2.jpg"></div>
         </div>
@@ -29,12 +30,20 @@ $title = "RentASnow - Accueil";
     <h1>Les news</h1>
     <?php foreach ($news as $onepieceofnews) { ?>
         <div class="row mt-4">
-            <div class="col-2"><?= date('d.M.Y', strtotime($onepieceofnews['date'])) ?> <?= "{$onepieceofnews["firstname"]} {$onepieceofnews["lastname"]}"?></div>
+            <div class="col-2"><?= date('d.M.Y', strtotime($onepieceofnews['date'])) ?> <?= "{$onepieceofnews["firstname"]} {$onepieceofnews["lastname"]}" ?></div>
             <h4 class="col-4"><?= $onepieceofnews['title'] ?></h4>
         </div>
         <div class="row ml-5"><?= $onepieceofnews['text'] ?></div>
     <?php } ?>
 </div>
+<?php if (isset($_SESSION["firstname"])) { ?>
+    <br><br>
+    <form action="index.php?action=addNew" method="post">
+        <input type="text" placeholder="Titre" name="title" required><br>
+        <textarea placeholder="Votre commentaire" name="text" required></textarea><br>
+        <button type="submit">Envoyer votre commentaire</button>
+    </form>
+<?php } ?>
 
 
 <script src="assets/carousel/jquery.carouFredSel-6.2.0-packed.js" type="text/javascript"></script>
