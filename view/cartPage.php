bbb4d492454937c0c7fd8fa61030dd555522fc1c
ChristopherPardo <47849638+ChristopherPardo@users.noreply.github.com>
2020-03-26 00:13:13 +0100

    Ajout données dans le pannier

--- view/cartPage.php
+++ view/cartPage.php
@@ -4,20 +4,18 @@ ob_start();
 ?>
 <h1>Votre panier</h1>
 <br>
-<hr>
-<img src="view/images/snows/small/B126.jpg">
-<h2>B126</h2>
-<h2>longueur : 160 cm</h2>
-<h2>Disponible</h2>
-<input type="button" value="Supprimer">
-<hr>
-<img src="view/images/snows/small/k409.jpg">
-<h2>K409</h2>
-<h2>longueur : 120 cm</h2>
-<h2>Indisponible</h2>
-<input type="button" value="Supprimer">
-<br><br><br>
-<input type="button" value="Commander">
+<?php foreach ($cart as $article) { ?>
+    <hr>
+    <img src="view/images/snows/small/<?= $article["photo"] ?>">
+    <h2><?= $article["model"] ?></h2>
+    <h2>longueur : <?= $article["length"] ?> cm</h2>
+    <?php if ($article["available"] == 1) { ?>
+        <h2>Disponible</h2>
+    <?php } else { ?>
+        <h2>Indisponible</h2>
+    <?php } ?>
+    <input type="button" value="Supprimer">
+<?php } ?>
 
 <?php
 $content = ob_get_clean();
